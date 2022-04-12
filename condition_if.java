f10e8395d7112da5dd731fe8a3fb9aee96f14dc9
ravirajy77 <98444975+ravirajy77@users.noreply.github.com>
2022-04-12 19:13:44 +0530

    Add files via upload

---
+++ condition_if.java
@@ -0,0 +1,22 @@
+package javafullcourse;
+
+import java.util.Scanner;
+
+public class condition_if {
+
+	public static void main(String[] args) {
+	int pwd;
+	System.out.print("Enter password :- ");
+	Scanner obj=new Scanner(System.in);
+	pwd=obj.nextInt();
+		if (pwd==1982 )
+		{ 
+			System.out.println(" Myname : Ravi");
+			System.out.println(" Myage : 26");
+			System.out.println(" Myadd : Palghar");
+			
+			
+		}
+	}
+
+}
